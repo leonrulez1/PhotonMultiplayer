7612471be96fd78cbdafef96edb7d1e5140525a7
Kaung Sit Hein <sithein99@gmail.com>
2020-02-15 22:22:12 +0800

    ADDED ASSETS AND 5 SCRIPTS

---
+++ GP Multiplayer/Assets/Scripts/Boss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public int health = 1000;
+    public GameObject deathEffect;
+    
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+            if (health <= 0)
+        {
+            Die();
+        }
+    }
+    
+    void Die ()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ GP Multiplayer/Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterControls : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 400f;                          // Amount of force added when the player jumps.
+    [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;          // Amount of maxSpeed applied to crouching movement. 1 = 100%
+    [Range(0, .3f)] [SerializeField] private float smoothMovement = .05f;  // How much to smooth out the movement
+    [SerializeField] private bool airControl = false;                         // Whether or not a player can steer while jumping;
+    [SerializeField] private LayerMask whatIsGround;                          // A mask determining what is ground to the character
+    [SerializeField] private Transform groundCheck;                           // A position marking where to check if the player is grounded.
+    [SerializeField] private Transform ceilingCheck;                          // A position marking where to check for ceilings
+    [SerializeField] private Collider2D crouchDisable;                // A collider that will be disabled when crouching
+
+    const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool grounded;            // Whether or not the player is grounded.
+    const float ceilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+    private Rigidbody2D rb;
+    private bool facingRight = true;  // For determining which way the player is currently facing.
+    private Vector3 velocity = Vector3.zero;
+
+    [Header("Events")]
+    [Space]
+
+    public UnityEvent onLandEvent;
+
+    //[System.Serializable]
+    //public class BoolEvent : UnityEvent<bool> { }
+
+    //public BoolEvent onCrouch;
+    private bool isCrouching = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        //if (onLandEvent == null)
+        //    onLandEvent = new UnityEvent();
+
+        //if (onCrouch == null)
+        //    onCrouch = new BoolEvent();
+    }
+
+    private void FixedUpdate()
+    {
+        bool wasGrounded = grounded;
+        grounded = false;
+
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                grounded = true;
+                if (!wasGrounded)
+                    onLandEvent.Invoke();
+            }
+        }
+    }
+
+
+    public void Move(float move, bool crouch, bool jump)
+    {
+        // If crouching, check to see if the character can stand up
+        if (crouch)
+        {
+            // If the character has a ceiling preventing them from standing up, keep them crouching
+            if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+            {
+                crouch = true;
+            }
+        }
+
+        //only control the player if grounded or airControl is turned on
+        if (grounded || airControl)
+        {
+
+            // If crouching
+            if (crouch)
+            {
+                if (!isCrouching)
+                {
+                    isCrouching = true;
+                    //onCrouch.Invoke(true);
+                }
+
+                // Reduce the speed by the crouchSpeed multiplier
+                move *= crouchSpeed;
+
+                // Disable one of the colliders when crouching
+                if (crouchDisable != null)
+                    crouchDisable.enabled = false;
+            }
+            else
+            {
+                // Enable the collider when not crouching
+                if (crouchDisable != null)
+                    crouchDisable.enabled = true;
+
+                if (isCrouching)
+                {
+                    isCrouching = false;
+                    //onCrouch.Invoke(false);
+                }
+            }
+
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+            // And then smoothing it out and applying it to the character
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, smoothMovement);
+
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !facingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (move < 0 && facingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+        // If the player should jump...
+        if (grounded && jump)
+        {
+            // Add a vertical force to the player.
+            grounded = false;
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
