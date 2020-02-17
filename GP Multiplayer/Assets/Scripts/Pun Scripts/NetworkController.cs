bdfa1ffdfe51bd8050e766895cff97c49aa3a712
Jesper <leonrulez1@gmail.com>
2020-02-17 16:53:06 +0800

    Photon Set up 1

---
+++ GP Multiplayer/Assets/Scripts/Pun Scripts/NetworkController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    //public string versionName = "0.1";
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Connecting to photon...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + "server!");
+    }
+
+    /*
+    private void OnJoinedLobby()
+    {
+        
+    }
+    */
+
+
+
+
+
+
+}
